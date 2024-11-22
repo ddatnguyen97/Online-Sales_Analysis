b514c7e027ff12760877601afa3ad1c04c1ea2a4
Dat Nguyen <ddatnguyen97@gmail.com>
2024-11-22 23:56:09 +0700

    Add pie charts of returned prods and payment method

--- Online Sales Dashboard.py
+++ Online Sales Dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
 df = pd.read_csv('online_sales_cleaned.csv')
 
@@ -16,22 +15,54 @@ with st.sidebar:
     else:
         filtered_df = df[df['Year'] == selected_year]
 
+    selected_method = st.selectbox('Payment Method', ['All'] + list(df['PaymentMethod'].unique()))
+    if selected_method != 'All':
+        filtered_df = df[df['PaymentMethod'] == selected_method]
+    else:
+        filtered_df = df
+
 total_sales = f"{filtered_df['TotalSales'].sum():,.2f}"       
 with st.container():
     col1, col2 = st.columns(2)
     col1.metric('Total Sales', total_sales)
     col2.metric('Quantity', filtered_df['Quantity'].sum())
-    # st.metric('Total Sales', filtered_df['TotalSales'].sum())
-    # st.metric('Quantity', filtered_df['Quantity'].sum())
-
-# with st.container():
-#     st.map(filtered_df.groupby('Country').size())
 
 with st.container():
     if selected_year == 'All':
+        st.subheader('Sales by Months')
         sales_by_month = filtered_df.groupby(['Year', 'Month'])['TotalSales'].sum().reset_index()
         sales_by_month_pivot = sales_by_month.pivot(index='Month', columns='Year', values='TotalSales')
         st.line_chart(sales_by_month_pivot)
     else:
+        st.subheader('Sales by Month')
         sales_by_month = filtered_df.groupby('Month')['TotalSales'].sum().reset_index()
         st.line_chart(sales_by_month.set_index('Month'))
+
+returned_products_df = filtered_df[filtered_df['ReturnStatus'] == 'Returned']
+percentage_of_returned_products = len(returned_products_df) / len(filtered_df) * 100
+successful_products_df = filtered_df[filtered_df['ReturnStatus'] != 'Returned']
+percentage_of_successful_products = len(successful_products_df) / len(filtered_df) * 100
+fig_1 = px.pie(
+        names=['Returned', 'Successful'],
+        values=[percentage_of_returned_products, 
+        percentage_of_successful_products],
+        color_discrete_sequence=['#8DECB4','#295F98', ],
+        hole=0.5,
+        )
+
+payment = filtered_df['PaymentMethod'].value_counts().sort_index()
+fig_2 = px.pie(
+        names=payment.index,
+        values=payment.values,
+        color_discrete_sequence=px.colors.qualitative.Set3,
+        hole=0.5,
+        )
+with st.container(): 
+    col1, col2 = st.columns(2) 
+    with col1: 
+        st.subheader('Percentage of Returned Products') 
+        st.plotly_chart(fig_1, use_container_width=True) 
+    
+    with col2: 
+        st.subheader('Percentage of Payment Method') 
+        st.plotly_chart(fig_2, use_container_width=True)
